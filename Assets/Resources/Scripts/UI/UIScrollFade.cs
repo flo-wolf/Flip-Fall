d820cc01427ecfd846d2d7bf0c374262e43ede68
Badeye <badeye@arcor.de>
2016-10-18 11:04:41 +0200

    Added ScrollRect fading component, which will be used to animate Objects leaving a scrollrect while the user scrolls.

---
+++ Assets/Resources/Scripts/UI/UIScrollFade.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// Calls an UIScrollElement attached to a scrollRect, whenever it leaves an area
+/// Checks wheter or not an UIScrollElement needs to be animated
+/// </summary>
+
+namespace FlipFall.UI
+{
+    public class UIScrollFade : MonoBehaviour
+    {
+        // the scrollRect
+        public ScrollRect scrollRect;
+
+        // ignores the insideArea property and uses the scrollable area of the ScrollRect instead
+        public bool useScollRectAsInsideArea = false;
+
+        // the area in which elements are "faded-in"
+        public Bounds insideBounds = new Bounds();
+
+        public bool visualizeBounds = true;
+
+        public List<UIScrollElement> scrollElements;
+
+        private bool dragging = false;
+
+        private void Start()
+        {
+            StartCoroutine(GetScrollElements());
+
+            if (useScollRectAsInsideArea)
+            {
+                insideBounds = new Bounds();
+                insideBounds.center = scrollRect.viewport.position;
+                insideBounds.size = scrollRect.viewport.sizeDelta;
+            }
+
+            if (visualizeBounds)
+            {
+                Gizmos.DrawWireCube(insideBounds.center, insideBounds.size);
+            }
+        }
+
+        private IEnumerator GetScrollElements()
+        {
+            scrollElements = new List<UIScrollElement>();
+            for (int i = 0; i < scrollRect.transform.childCount; i++)
+            {
+                if (scrollRect.transform.GetChild(i).GetComponent<UIScrollElement>() != null)
+                {
+                    scrollElements.Add(scrollRect.transform.GetChild(i).GetComponent<UIScrollElement>());
+                }
+            }
+            yield break;
+        }
+
+        private void FixedUpdate()
+        {
+            // check the validity of the inside statement
+            if (dragging)
+            {
+                foreach (UIScrollElement element in scrollElements)
+                {
+                    if (!IsInside(element.transform.position))
+                    {
+                    }
+                    // begin fading animations if needed
+                }
+            }
+        }
+
+        //checks if a position is inside the scrollArea
+        private bool IsInside(Vector3 position)
+        {
+            if (insideBounds.Contains(position))
+                return true;
+            return false;
+        }
+
+        // Event Listener, called when the elements get moved
+        private void OnDrag()
+        {
+            dragging = true;
+        }
+
+        private void OnRelease()
+        {
+            dragging = false;
+        }
+    }
+}
---
+++ Assets/Resources/Scripts/UI/UIScrollFadeElement.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Needs to be attached to a GameObject which s child to a ScrollRect and thus effected by touch input.
+/// Needs a Referenced Animator to function.
+/// A "fade" trigger gets called in the Animator and causes Fade-in and -outs.
+/// Thus each element can have different or even none fading animations.
+///
+/// This script gets called by the UIScrollFade script.
+/// </summary>
+
+namespace FlipFall.UI
+{
+    public class UIScrollElement : MonoBehaviour
+    {
+        // Fading Animator
+        public Animator animator;
+
+        // Controls wether or not is is needed to start a fading animation
+        // if an element is outside the scrollArea, but set to inside, a fadeout will be called - setting it to false, to prevent more fades from happening
+        public bool inside;
+
+        // Use this for initialization
+        private void Start()
+        {
+            if (animator == null)
+            {
+                animator = gameObject.GetComponent<Animator>();
+            }
+        }
+
+        public void FadeIn()
+        {
+            animator.SetTrigger("fadein");
+        }
+
+        public void FadeOut()
+        {
+            animator.SetTrigger("fadeout");
+        }
+
+        public void Destroy()
+        {
+            GameObject.Destroy(this);
+        }
+    }
+}
