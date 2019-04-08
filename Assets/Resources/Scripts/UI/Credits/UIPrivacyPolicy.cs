a6b4f54bb3e02b751d7fcc9dd80b3840adab9652
Badeye <badeye@arcor.de>
2019-04-08 21:12:47 +0200

    Added a link to the privacy policy in the information/credits menu (little i next to the title). Fixed UI scaling in the Tutorial menu. Updated License.

---
+++ Assets/Resources/Scripts/UI/Credits/UIPrivacyPolicy.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIPrivacyPolicy : MonoBehaviour {
+
+	public string fileName = "PrivacyPolicyURL";
+
+	private void Start()
+	{
+	}
+
+	public void OpenPrivacyPolicyURL()
+	{
+		Application.OpenURL(GetURL());
+	}
+
+	public string GetURL()
+	{
+		TextAsset privacyPolicyFile = Resources.Load<TextAsset>(fileName);
+		
+		string privacyPolicy = privacyPolicyFile.text;
+		return privacyPolicy;
+	}
+}
